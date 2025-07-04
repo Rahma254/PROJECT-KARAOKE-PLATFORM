24e8ee40a4c1c17ec87dddec596fa13e95c6bf3f
Rahma254 <rahmasiska661@gmail.com>
2025-07-04 14:27:14 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import HomePage from './pages/home.page';
+
+const App = () => {
+  return <HomePage />;
+};
+
+export default App;
